4dc30d86a3c848fbe1fc4a7769f42a9def979dd5
mdenyse-en <shal.s.a@yandex.ru>
2024-07-08 15:25:03 +0300

    delete target folder

--- target/classes/scripts/userInfoPage.js
+++
@@ -1,8 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', async function () {
-    fetch('/api/current')
-        .then(response => response.json())
-        .then(user => fillUserInfo(user))
-        .then(user => fillCurrentUserInfo(user))
-        .catch(error => console.log(error))
-})
